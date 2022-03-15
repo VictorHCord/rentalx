525b4b0b9e897cf52de2c56579b834b481875586
VictorHCord <victorhugocordeiro1997@gmail.com>
2022-03-15 17:19:56 -0300

    refactor(files): add singleton pattern
    add single pattern in my application and refactor old files

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -6,18 +6,29 @@ import { ICategoriesRepository, ICreateCategoryDTO } from './ICategoriesReposito
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[]  = [];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
   }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
+
   create({ name, description} : ICreateCategoryDTO): void {
     const category: Category = new Category();
-  
+
     Object.assign(category, {
-      name, 
+      name,
       description,
       created_at: new Date()
     });
-  
+
     this.categories.push(category);
   }
 
