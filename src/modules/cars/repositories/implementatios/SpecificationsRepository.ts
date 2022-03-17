4f4fd4a4f77e7579007ebaa446d9fbd6083eeb8c
VictorHCord <victorhugocordeiro1997@gmail.com>
2022-03-16 21:53:55 -0300

    refactor(file): refactor specification category

--- src/modules/cars/repositories/implementatios/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementatios/SpecificationsRepository.ts
@@ -1,29 +1,28 @@
 import { Specification } from "modules/cars/entities/Specification";
+import { getRepository, Repository } from "typeorm";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from '../ISpecificationRepository';
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       name,
-      description,
-      create_at: new Date()
+      description
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(specification =>
-      specification.name === name
-      );
+  async findByName(name: string): Promise<Specification> {
+    const specification = this.repository.findOne({
+      name,
+    });
+
       return specification;
   }
 
--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -2,12 +2,12 @@ import { Request, Response } from "express";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 import { container } from 'tsyringe';
 class CreateSpecificationController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
     const createSpecificationUseCase = container.resolve(CreateSpecificationUseCase)
 
-   createSpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
 
   return response.status(201).send();
   }
