04f07d158fbc317799744f897ac8b21fb2177b2e
VictorHCord <victorhugocordeiro1997@gmail.com>
2022-03-18 14:11:51 -0300

    feat(file): created avatar

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,12 +11,14 @@ class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async create({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+  async create({ name, email, driver_license, password, avatar, id }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       password,
       driver_license,
+      avatar,
+      id
     });
 
     await this.repository.save(user);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
 import { CreateUserController } from 'modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from 'modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import multer from 'multer';
+import uploadConfig from "../config/upload";
 
 const usersRoutes = Router();
 
-const createUserController = new CreateUserController();
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
-usersRoutes.post('/', createUserController.handle)
+const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+usersRoutes.use(ensureAuthenticated)
+
+usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch("/avatar", uploadAvatar.single("avatar") ,updateUserAvatarController.handle);
 
 export { usersRoutes };
