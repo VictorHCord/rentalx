5373bd650ef8756fecc7bc381a3ed3b64227a1c5
VictorHCord <victorhugocordeiro1997@gmail.com>
2022-03-18 14:22:16 -0300

    feat(file): add avatar and delete avatar
    b

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { IUsersRepository } from "modules/accounts/repositories/IUsersRepository";
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from '../../../../utils/file';
 
 interface IRequest {
   user_id: string;
@@ -15,6 +16,9 @@ class UpdateUserAvatarUseCase {
   async execute({ avatar_file, user_id }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_file;
 
     await this.usersRepository.create(user)
